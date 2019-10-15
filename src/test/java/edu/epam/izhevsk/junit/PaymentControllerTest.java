afcd9dd17ceb9b034498cd4ad8b0c8404d6821aa
Ivan <Kishkan91@gmail.com>
2019-10-15 23:56:16 +0400

    task3 is finished

--- src/test/java/edu/epam/izhevsk/junit/PaymentControllerTest.java
+++ src/test/java/edu/epam/izhevsk/junit/PaymentControllerTest.java
@@ -1,22 +1,55 @@
 package edu.epam.izhevsk.junit;
 
-import org.hamcrest.CustomMatcher;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import static org.mockito.AdditionalMatchers.*;
-import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
-
-
 
 
 public class PaymentControllerTest {
 
- 
+    @Mock
+    AccountService accountService;
+    @Mock
+    DepositService depositService;
+
+    @InjectMocks
+    PaymentController paymentController;
+
+
+    @Test
+    public void testDepositSuccessful() throws InsufficientFundsException {
+        MockitoAnnotations.initMocks(this);
+        when(accountService.isUserAuthenticated(100L)).thenReturn(true);
+        when(accountService.isUserAuthenticated(AdditionalMatchers.not(eq(100L)))).thenReturn(false);
+
+        paymentController.deposit(50L, 100L);
+        verify(accountService, Mockito.times(1)).isUserAuthenticated(100L);
+        verify(depositService, Mockito.times(1)).deposit(50L, 100L);
+    }
+
+    @Test
+    public void testDepositInvalidUser() {
+        MockitoAnnotations.initMocks(this);
+        when(accountService.isUserAuthenticated(100L)).thenReturn(true);
+        when(accountService.isUserAuthenticated(AdditionalMatchers.not(eq(100L)))).thenReturn(false);
+
+        assertThrows(SecurityException.class, (() -> paymentController.deposit(50L, 200L)));
+    }
+
+    @Test
+    public void testDepositInvalidAmount() throws InsufficientFundsException {
+        MockitoAnnotations.initMocks(this);
+        when(accountService.isUserAuthenticated(100L)).thenReturn(true);
+        when(accountService.isUserAuthenticated(AdditionalMatchers.not(eq(100L)))).thenReturn(false);
+        Mockito.when(depositService.deposit(AdditionalMatchers.gt(100L), ArgumentMatchers.any()))
+                .thenThrow(InsufficientFundsException.class);
+
+        assertThrows(InsufficientFundsException.class, (() -> paymentController.deposit(150L, 100L)));
+    }
+
+
 }
